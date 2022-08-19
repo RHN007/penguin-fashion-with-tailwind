db6a33426fb473ecc78b4978be904935a0fab426
RHN007 <rakibul.nayon@outlook.com>
2022-08-19 11:03:43 +0600

    adding js-file

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById('buy-now').addEventListener('click', function(){
+    const titleChange = document.getElementById('penguin-title'); 
+    titleChange.innerText = "I AM PENGUIN"
+})
